447594e1096cb89bac7cb65f881b5e78a9b101d4
Kashirin pro <maksimus_kashiri@mail.ru>
2026-06-03 21:36:35 +0300

    Task 3

--- main.py
+++ main.py
@@ -16,6 +16,35 @@ class Student:
         else:
             return 'Ошибка'
 
+    def average_grade(self):
+        all_grades = []
+        for grades in self.grades.values():
+            all_grades += grades
+        if len(all_grades) == 0:
+            return 0
+        return sum(all_grades) / len(all_grades)
+
+    def __str__(self):
+        return f'Имя: {self.name}\n' \
+               f'Фамилия: {self.surname}\n' \
+               f'Средняя оценка за домашние задания: {self.average_grade()}\n' \
+               f'Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n' \
+               f'Завершенные курсы: {", ".join(self.finished_courses)}'
+
+    def __lt__(self, other):
+        if isinstance(other, Student):
+            return self.average_grade() < other.average_grade()
+        return 'Ошибка'
+
+    def __gt__(self, other):
+        if isinstance(other, Student):
+            return self.average_grade() > other.average_grade()
+        return 'Ошибка'
+
+    def __eq__(self, other):
+        if isinstance(other, Student):
+            return self.average_grade() == other.average_grade()
+        return 'Ошибка'
 
 class Mentor:
     def __init__(self, name, surname):
@@ -23,11 +52,38 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
-
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
+
+    def average_grade(self):
+        all_grades = []
+        for grades in self.grades.values():
+            all_grades += grades
+        if len(all_grades) == 0:
+            return 0
+        return sum(all_grades) / len(all_grades)
+
+    def __str__(self):
+        return f'Имя: {self.name}\n' \
+               f'Фамилия: {self.surname}\n' \
+               f'Средняя оценка за лекции: {self.average_grade()}'
+
+    def __lt__(self, other):
+        if isinstance(other, Lecturer):
+            return self.average_grade() < other.average_grade()
+        return 'Ошибка'
+
+    def __gt__(self, other):
+        if isinstance(other, Lecturer):
+            return self.average_grade() > other.average_grade()
+        return 'Ошибка'
+
+    def __eq__(self, other):
+        if isinstance(other, Lecturer):
+            return self.average_grade() == other.average_grade()
+        return 'Ошибка'
 
 
 class Reviewer(Mentor):
@@ -40,18 +96,28 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
+    def __str__(self):
+        return f'Имя: {self.name}\n' \
+               f'Фамилия: {self.surname}'
 
-lecturer = Lecturer('Иван', 'Иванов')
-reviewer = Reviewer('Пётр', 'Петров')
-student = Student('Алёхина', 'Ольга', 'Ж')
+some_reviewer = Reviewer('Some', 'Buddy')
+some_lecturer = Lecturer('Some', 'Buddy')
+some_student = Student('Ruoy', 'Eman', 'your_gender')
 
-student.courses_in_progress += ['Python', 'Java']
-lecturer.courses_attached += ['Python', 'C++']
-reviewer.courses_attached += ['Python', 'C++']
+some_student.courses_in_progress += ['Python', 'Git']
+some_student.finished_courses += ['Введение в программирование']
 
-print(student.rate_lecture(lecturer, 'Python', 7))
-print(student.rate_lecture(lecturer, 'Java', 8))
-print(student.rate_lecture(lecturer, 'С++', 8))
-print(student.rate_lecture(reviewer, 'Python', 6))
+some_lecturer.courses_attached += ['Python', 'Git']
+some_reviewer.courses_attached += ['Python', 'Git']
 
-print(lecturer.grades)
+some_student.rate_lecture(some_lecturer, 'Python', 10)
+some_student.rate_lecture(some_lecturer, 'Git', 9.8)
+
+some_reviewer.rate_hw(some_student, 'Python', 10)
+some_reviewer.rate_hw(some_student, 'Git', 9.8)
+
+print(some_reviewer)
+print()
+print(some_lecturer)
+print()
+print(some_student)
